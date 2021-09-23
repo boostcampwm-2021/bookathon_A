5b60141fbb74b210cac52bd89669466184860093
imnotmoon <imnotmoon@gmail.com>
2021-09-24 00:04:54 +0900

    [feat] 질문 페이지 이스터에그 추가

--- public/javascripts/question/question.js
+++ public/javascripts/question/question.js
@@ -6,7 +6,6 @@ const BASE_API_URL = "http://localhost:3000/question";
 
 // doms
 const $target = () => document.querySelector("#app");
-const $answers = () => document.querySelectorAll(".answer");
 
 export const initQuestion = async ({ num, answer1, answer2 }) => {
 	setStore({ currentPage: num });
@@ -24,13 +23,40 @@ const toNextQuestion = (data) => {
 	render(data);
 };
 
+const onClickEasterEggHandler = (e) => {
+	const $answer = e.target.closest(".answer-easter");
+	if (!$answer) return;
+
+	e.stopImmediatePropagation();
+	const newAnswer = $answer.cloneNode(true);
+	console.log(newAnswer);
+	$answer.style.visibility = "hidden";
+
+	const frame = document.querySelector(".question-content");
+	frame.appendChild(newAnswer);
+	const [frameHeight, frameWidth] = [
+		parseInt(window.getComputedStyle(frame).height),
+		parseInt(window.getComputedStyle(frame).width),
+	];
+	console.log(frameHeight, frameWidth);
+
+	newAnswer.style.position = "absolute";
+	newAnswer.style.top = `${Math.random() * frameHeight}px`;
+	newAnswer.style.left = `${Math.random() * frameWidth}px`;
+};
+
 // eventhandlers
 const onClickAnswer = async (target) => {
 	const $answer = target.closest(".answer");
 	if (!$answer) return;
 	const picked = $answer.dataset.answer;
 	const [result] = await fetchQuestion(getStore().currentQuestionNumber, picked);
-	console.log(result);
+
+	if (getStore().currentQuestionNumber === 0 && picked == 1) {
+		setStore({ currentQuestionNumber: 99 });
+		toNextQuestion({ num: 99, answer1: "아뇨, 개발에 관심이 생겼습니다.", answer2: "그럼 이만!" });
+		return;
+	}
 
 	if (!result.name) {
 		setStore({ currentQuestionNumber: result.num });
@@ -41,8 +67,20 @@ const onClickAnswer = async (target) => {
 	}
 };
 
+const onClickPrevious = (target) => {
+	const button = target.closest(".pagenation-button");
+	if (!button) return;
+	console.log("hihi");
+};
+
 const addEvents = () => {
+	$target().addEventListener("mouseover", (e) => {
+		onClickEasterEggHandler(e);
+	});
 	$target().addEventListener("click", ({ target }) => {
 		onClickAnswer(target);
+	});
+	$target().addEventListener("click", ({ target }) => {
+		onClickPrevious(target);
 	});
 };
