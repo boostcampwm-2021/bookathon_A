4bcda15640aee45a0d3c3a4984332f50478bceb2
jyh <jeonyh0521@gmail.com>
2021-09-23 13:08:49 +0900

    [ADD] nedb 설정 추가

--- config/database.js
+++ config/database.js
@@ -1,1 +1,28 @@
-// database connect
+const database = require('nedb');
+
+const questionDB = new database({
+    filename: './database/databaseFile/questionDB.db',
+    autoload: true
+});
+
+// const insert = function(DB, data) {
+//     DB.insert(data, function(err, newDoc) {});
+// }
+
+// const select = function(DB) {
+//     return new Promise(resolve => {
+//         DB.find({}, (err, docs) => resolve(docs));
+//     });
+// }
+
+// const paymentDelete = function(payment) {
+//     paymentDB.remove({ payment: payment }, {}, function(err, numRemoved){});
+// }
+
+// const update = (DB, originData, updateData) => {
+//     return new Promise(resolve => {
+//         DB.update(originData, updateData, (err, docs) => resolve());
+//     });
+// }
+
+// module.exports = { contentDB, paymentDB, insert, select, paymentDelete, update };
