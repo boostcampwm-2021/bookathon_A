5930bc965a63efa752dd7c3580fc6d119809d116
jyh <jeonyh0521@gmail.com>
2021-09-24 01:27:02 +0900

    [ADD] 결과 페이지 디자인 작성

--- public/javascripts/result/render-result.js
+++ public/javascripts/result/render-result.js
@@ -1,40 +1,36 @@
 const target = () => app;
 
 const template = (result) => `
-    <div class="result">
-        <div class="result-header">
-            나는 ${result.name} 개발자입니다.
-        </div>
-        <div class="result-image">
-            <img src="../public/src/image/thumnail/${result.image}.png" />
-        </div>    
-        <div class="result-content">
-            ${result.short}
-        </div>
-        <div class="result-content">
-            ${result.content}
-        </div>
-        <div class="result-content">
-            <div class="result-success">
-                <div class="result-title">
-                    성공사례
-                </div>
-                <div class="result-list">`+
-    result.success.reduce((p, c) => {
-        return p + `<img src="../public/src/image/success/${c}.png" />`;
-    }, "") + `
-                </div>
+    <div class="result-page">
+        <div class="result-window">
+            <div class="header">
+                <div></div><div></div><div></div>
             </div>
-            <div class="result-language">
-                <div class="result-title">
-                    관련 언어
+            <section class="result">
+                <div class="result-name">${result.name}</div>
+                <div class="result-image">
+                    <img src="../../src/image/thumnail/${result.image}.png" />
                 </div>
-                <div class="result-list">`+
-    result.language.reduce((p, c) => {
-        return p + `<img src="../public/src/image/language/${c}.png" />`;
-    }, "") + `
+                <div class="result-short">${result.short}</div>
+                <div class="result-content">${result.content}</div>
+                <div class="result-success">
+                    <div class="title">예시</div>
+                    <div class="result-list">`+
+                    result.success.reduce((p, c) => {
+                        return p + `<img src="../../src/image/success/${c}.png" />`;
+                    }, "") + `
+                    </div>
                 </div>
-            </div>
+                <div class="result-language">
+                    <div class="title">사용 언어</div>
+                    <div class="result-list">`+
+                    result.language.reduce((p, c) => {
+                        return p + `<img src="../../src/image/language/${c}.png" />`;
+                    }, "") + `
+                    </div>
+                </div>
+            </section>
+            <div class="result-temp"></div>        
         </div>
     </div>
 `;
