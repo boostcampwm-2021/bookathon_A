0880d3e582543014cf1f26cc51e9a7b371b9d236
imnotmoon <imnotmoon@gmail.com>
2021-09-24 11:49:11 +0900

    [feat] 결과 페이지 다시하기 버튼 추가

--- public/javascripts/result/result.js
+++ public/javascripts/result/result.js
@@ -1,4 +1,5 @@
 import { render } from "./render-result.js";
+import { initPage } from "../start/start.js";
 import { imageSlider } from "./animation-result.js";
 import { setStore, getStore } from "../store.js";
 
@@ -6,4 +7,14 @@ export const renderResult = (result) => {
 	render(result);
 	imageSlider(document.querySelector(".result-success .result-list"));
 	imageSlider(document.querySelector(".result-language .result-list"));
+
+	addEvents();
+};
+
+const onClickRetry = (e) => {
+	initPage();
+};
+
+const addEvents = () => {
+	document.querySelector("#app > header").addEventListener("click", onClickRetry);
 };
