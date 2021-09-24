7cc50778df180ce8c1ae76ff3ea3eae015f93a80
origin <chldlsrb1000@naver.com>
2021-09-24 10:11:52 +0900

    ⚙: 결과창 애니메이션 추가 결과 이미지 fadein - fadeout 애니메이션. 추가로 드래그 방지 css 추가함.

--- public/javascripts/result/render-result.js
+++ public/javascripts/result/render-result.js
@@ -10,7 +10,7 @@ const template = (result) =>
                 <div>
                     <div class="result-name">저는 ${result.name} 개발자 입니다.</div>
                     <div class="result-image">
-                        <img src="../../src/image/thumnail/${result.image}.png" />
+                        <img width="300px" src="../../src/image/thumbnail/${result.image}.png" />
                     </div>
                     <div class="result-short">${result.short}</div>
                     <div class="result-content">${result.content}</div>
@@ -18,19 +18,20 @@ const template = (result) =>
                 <div>
                     <div class="result-success">
                         <div class="title">예시</div>
+                        
                         <div class="result-list">` +
-	result.success.reduce((p, c) => {
-		return p + `<img src="../../src/image/success/${c}.png" />`;
-	}, "") +
+            result.success.reduce((p, c) => {
+                return p + `<img class="result-img" src="../../src/image/success/${c}.png" />`;
+            }, "") +
 	`
                         </div>
                     </div>
                     <div class="result-language">
                         <div class="title">사용 언어</div>
                         <div class="result-list">` +
-	result.language.reduce((p, c) => {
-		return p + `<img src="../../src/image/language/${c}.png" />`;
-	}, "") +
+                result.language.reduce((p, c) => {
+                    return p + `<img class="result-img" src="../../src/image/language/${c}.png" />`;
+                }, "") +
 	`
                         </div>
                     </div>
