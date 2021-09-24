094b748e2c7f310264c4fa57c07898b2f5d45cdb
ChipmunkForLove <chipmunk4lov@gmail.com>
2021-09-24 09:46:49 +0900

    저장 안한 것들 추가 커밋

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,12 +6,6 @@ const $body = () => document.querySelector("body");
 
 const init = () => {
 	$body().innerHTML = `<div id="app"></div>`;
-	/*initQuestion({
-		num: 0,
-		question: "미래의 개발자님, 안녕하세요!",
-		answer1: "사이트 잘못 들어왔어요. 나갈래요",
-		answer2: "개발에 관심이 생겼어요.",
-	});*/
 	initPage();
 };
 
